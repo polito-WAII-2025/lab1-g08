4a9344e9a44609902eb7cbdc8df38d0e86d1e53d
Alice Santoro s324739 <s324739@studenti.polito.it>
2025-03-13 15:22:24 +0100

    feat: custom parameters yml parsing

--- RouteAnalyzer/src/main/kotlin/Main.kt
+++ RouteAnalyzer/src/main/kotlin/Main.kt
@@ -3,4 +3,7 @@ package it.polito.g08
 fun main() {
     val points = Waypoint.fromCSV("/waypoints.csv")
     println(points)
+
+    val parameters = CustomParameters.fromYAML("/custom-parameters.yml")
+    println(parameters)
 }
