c614a98c29306c7865fca372740b0b89e07d551b
Stefano Di Leo <>
2025-03-14 10:49:27 +0100

    feat: implement waypoint functions

--- RouteAnalyzer/src/main/kotlin/Main.kt
+++ RouteAnalyzer/src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 package it.polito.g08
 
+import com.uber.h3core.util.LatLng
+
 fun main() {
     val points = Waypoint.fromCSV("/waypoints.csv")
-    println(points)
+    println("maxDistanceFromStart: ${points.maxDistanceFromStart()}")
+    println("waypointsOutsideGeofence: ${points.waypointsOutsideGeofence(
+        LatLng(45.041899, 7.649921),
+        1.0
+    )}")
+    val res = points.mostFrequentedArea(1.0)
+    println("mostFrequentedArea: $res")
+    println("Cittadella design: ${points.pointsInArea(LatLng(res.first.latitude, res.first.longitude), 1.0)}")
+    println("mostFrequentedAreaPrecise: ${points.mostFrequentedAreaPrecise(1.0)}")
+    //println(points)
 }
--- RouteAnalyzer/src/main/kotlin/Waypoints.kt
+++ RouteAnalyzer/src/main/kotlin/Waypoints.kt
@@ -1,28 +1,164 @@
 package it.polito.g08
 
+import com.uber.h3core.AreaUnit
+import com.uber.h3core.H3Core
+import com.uber.h3core.LengthUnit
+import com.uber.h3core.util.LatLng
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
+import org.jetbrains.kotlinx.dataframe.api.ParserOptions
+import org.jetbrains.kotlinx.dataframe.api.add
 import org.jetbrains.kotlinx.dataframe.api.cast
 import org.jetbrains.kotlinx.dataframe.api.toList
 import org.jetbrains.kotlinx.dataframe.io.readCSV
+import java.util.*
+import kotlin.math.*
+
+private object H3Singleton {
+    val h3: H3Core = H3Core.newInstance()
+}
+
+const val EARTH_RADIUS: Double = 6371.0088 // In Km
 
 @DataSchema(isOpen = false)
 data class Waypoint(
     val timestamp: Double,
     val latitude: Double,
     val longitude: Double,
+    val cell: Long
 ) {
-
     companion object {
-        fun fromCSV(resourcePath: String): List<Waypoint> {
+        fun fromCSV(resourcePath: String, cellResolution: Int = 15): List<Waypoint> {
+            val h3 = H3Singleton.h3
             val inputStream = object {}.javaClass.getResourceAsStream(resourcePath)
                 ?: throw IllegalArgumentException("File not found: $resourcePath")
+
             return DataFrame.readCSV(
                 inputStream,
                 header = listOf("timestamp", "latitude", "longitude"),
-                delimiter = ';'
-
-            ).cast<Waypoint>().toList()
+                delimiter = ';',
+                // The parser uses the system's locale to parse numbers and dates, causing problems
+                // when parsing Doubles (the CSV uses dots to represent decimals), so we force using
+                // the US locale to prevent problems.
+                parserOptions = ParserOptions(Locale.US)
+            ).add("cell") {
+                h3.latLngToCell("latitude"(), "longitude"(), cellResolution)
+            }.cast<Waypoint>().toList()
         }
+    }
+}
+
+/**
+ * Finds the furthest waypoint from the path's starting waypoint
+ *
+ * @param earthRadius the radius of the Earth in Km
+ * @return A `Pair` containing the furthest waypoint and its distance from the starting waypoint, or `null` if
+ * the `List` does not contain enough waypoints
+ */
+fun List<Waypoint>.maxDistanceFromStart(earthRadius: Double = EARTH_RADIUS): Pair<Waypoint, Double>? {
+    val h3 = H3Singleton.h3
+    val startingPoint = this.firstOrNull()?.let {
+        LatLng(it.latitude, it.longitude)
+    } ?: return null
+
+    return this.map {
+        val currentPoint = LatLng(it.latitude, it.longitude)
+        val distance = h3.greatCircleDistance(startingPoint, currentPoint, LengthUnit.rads) * earthRadius
+        Pair(it, distance)
+    }.maxByOrNull {
+        it.second
+    }
+}
+
+/**
+ * Computes the number of points outside the specified geofence
+ *
+ * @param geofenceCenter the center of the geofence
+ * @param geofenceRadius the radius of the geofence in Km
+ * @param earthRadius the radius of the Earth in Km
+ * @return Count of points outside geofence
+ */
+fun List<Waypoint>.waypointsOutsideGeofence(geofenceCenter: LatLng, geofenceRadius: Double, earthRadius: Double = EARTH_RADIUS): Int {
+    val h3 = H3Singleton.h3
+    return this.count {
+        val currentPoint = LatLng(it.latitude, it.longitude)
+        h3.greatCircleDistance(geofenceCenter, currentPoint, LengthUnit.rads) * earthRadius > geofenceRadius
+    }
+}
+
+/**
+ * Finds the coordinates of the most frequented area.
+ * Uses Uber's H3 geospatial indexing system. Provides an approximate result in linear time.
+ *
+ * @param areaRadius the desired minimum area radius in Km
+ * @return A `Pair` containing the coordinates of the most frequented area and the corresponding number of waypoints
+ */
+fun List<Waypoint>.mostFrequentedArea(areaRadius: Double): Pair<Waypoint, Int> {
+    val h3 = H3Singleton.h3
+    val areaKm2 = Math.PI * areaRadius.pow(2.0)
+    val closestResolution = (0..15).reduce { acc, i ->
+        val avgHexArea = h3.getHexagonAreaAvg(i, AreaUnit.km2)
+        if (avgHexArea > areaKm2)
+            i
+        else
+            return@reduce acc
+    }
+
+    return this.groupBy {
+        // Each resolution corresponds to 1 Nibble (4 bits) in the cell address
+        it.cell and (Long.MAX_VALUE shl ((closestResolution - 1 ) * 4))
+    }.map {
+        Pair(it.key, it.value.size)
+    }.maxBy {
+        it.second
+    }.let {
+        val ret = h3.cellToLatLng(it.first).let { latLng ->
+            Waypoint(0.0, latLng.lat, latLng.lng, it.first)
+        }
+        Pair(ret, it.second)
+    }
+}
+
+/**
+ * Finds the coordinates of the most frequented area.
+ * Uses a slow precise search of quadratic time
+ *
+ * @param areaRadius the desired minimum area radius in Km
+ * @param earthRadius the radius of the Earth in Km
+ * @return A `Pair` containing the coordinates of the most frequented area and the corresponding number of waypoints
+ */
+fun List<Waypoint>.mostFrequentedAreaPrecise(areaRadius: Double, earthRadius: Double = EARTH_RADIUS): Pair<Waypoint, Int> {
+    val h3 = H3Singleton.h3
+    return this.map { waypoint ->
+        val currentPoint = LatLng(waypoint.latitude, waypoint.longitude)
+        val outside = this.fold(0) {acc, w ->
+            val wp = LatLng(w.latitude, w.longitude)
+            if (h3.greatCircleDistance(currentPoint, wp, LengthUnit.rads) * earthRadius > areaRadius)
+                acc
+            else
+                acc + 1
+        }
+        Pair(waypoint, outside)
+    }.maxBy {
+        it.second
+    }
+}
+
+/**
+ * Finds how many points are within an area
+ *
+ * @param point the center of the area
+ * @param areaRadius the desired minimum area radius in Km
+ * @param earthRadius the radius of the Earth in Km
+ * @return The number of points in the area
+ */
+fun List<Waypoint>.pointsInArea(point: LatLng, areaRadius: Double, earthRadius: Double = EARTH_RADIUS): Int {
+    val h3 = H3Singleton.h3
+    return this.fold(0) {acc, w ->
+        val wp = LatLng(w.latitude, w.longitude)
+        if (h3.greatCircleDistance(point, wp, LengthUnit.rads) * earthRadius > areaRadius)
+            acc
+        else
+            acc + 1
     }
 }
